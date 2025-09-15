8b54b3b64130e89918c518d9fd16fd4c94395f7a
fetrarison <hei.fetra.2@gmail.com>
2025-09-15 22:36:13 +0300

    chore : creation  routage

--- public/index.php
+++ public/index.php
@@ -1,55 +1,29 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+function formatUrl($string) {
+    $string = strtolower($string);
+    $string = preg_replace('~[^a-z0-9 -]~', '', $string); 
+    $string = preg_replace('~[ -]+~', '-', $string); 
+    return $string;
 }
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
 
-require __DIR__.'/../vendor/autoload.php';
+$requestUri = $_SERVER['REQUEST_URI'];
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
+$segments = explode('/', trim($requestUri, '/'));
+$candidateName = $segments[0]; 
+$pageSlug = isset($segments[1]) ? $segments[1] : ''; 
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$candidateNameFormatted = formatUrl($candidateName);
+$pageSlugFormatted = formatUrl($pageSlug);
 
-$kernel = $app->make(Kernel::class);
+if ($candidateNameFormatted === 'fetrarison' && $pageSlugFormatted === 'completez-votre-reservation') {
+    include('fetrarison-completez-votre-reservation.html');
+} elseif ($candidateNameFormatted === 'fetrarison' && $pageSlugFormatted === 'paiement') {
+    include('fetrarison-paiement.html');
+} else {
+   
+    echo "Page non trouvée.";
+}
 
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+?>
